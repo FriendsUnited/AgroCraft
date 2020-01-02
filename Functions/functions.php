d8abe4a3c0f0dc24c6c6685c63dfc52fae0a4b7a
Elysian Spirit <abhig0209@gmail.com>
2020-01-02 18:40:33 +0530

    test update for homepage

--- Functions/functions.php
+++ Functions/functions.php
@@ -14,8 +14,8 @@
         while ($row_cat = mysqli_fetch_array($run_query)) {
             $product_type = $row_cat['product_type'];
 
-            // echo "<li><a href = '#'>$product_type</a></li>";
-            echo $product_type;
+            
+            echo "<option class='items1'>$product_type</option>";
         }
     }
 
@@ -30,8 +30,7 @@
         while ($row_cat = mysqli_fetch_array($run_query)) {
             $product_type = $row_cat['product_type'];
 
-            // echo "<li><a href = '#'>$product_type</a></li>";
-            echo $product_type;
+            echo "<option class='items1'>$product_type</option>";
         }
     }
 
@@ -46,8 +45,7 @@
         while ($row_cat = mysqli_fetch_array($run_query)) {
             $product_type = $row_cat['product_type'];
 
-            // echo "<li><a href = '#'>$product_type</a></li>";
-            echo $product_type;
+            echo "<option class='items1'>$product_type</option>";
         }
     }
     function getFarmerProducts() {
@@ -61,13 +59,14 @@
         global $con;
         $query = "select * from products  order by RAND() LIMIT 0,4";
         $run_query = mysqli_query($con,$query);
+        echo "<br>";
         while($rows = mysqli_fetch_array($run_query)){
             $product_title = $rows['product_title'];
             $product_image = $rows['product_image'];
             $product_price = $rows['product_price'];
             $product_delivery = $rows['product_delivery'];
 
-<<<<<<< HEAD
+           
             echo "<div class='example'>
                     <div class='wrapper'>
                         <div class='inputwrapper'>
@@ -80,20 +79,6 @@
                             <button class='addtocart'>ADD TO CART <i class='fas fa-shopping-cart' style=' background-color:#FFD700'></i></button><br><br>
                         </div>      
                 </div></div>";
-=======
-            echo "<div class='wrapper'>
-                    <div class='inputwrapper'>
-                        <img src='./Admin/product_images/$product_image' alt= 'Image Not Available' style='height: 100px; width: 100px;'><br><br>
-                        <label>$product_title</label><br>
-                        <label>PRICE:- $product_price Rs/kg</label><br>	
-                        <label id='shop2'>Delivery by Farmer</label><br>Qty:-
-                        <input class='numberinput' type='number' name='number'  >
-            
-                        <button class='addtocart'>ADD TO CART <i class='fas fa-shopping-cart' style=' background-color:#FFD700'></i></button>
-                        <br><br>
-                    </div >      
-                </div><br><br>";
->>>>>>> 54e3b7302c6e789979c9909ab1efe9b38096ed42
 
         }
     }

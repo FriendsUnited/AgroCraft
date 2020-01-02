79edcd8457a95d21403460549b729541f83d1987
Elysian Spirit <abhig0209@gmail.com>
2020-01-02 09:18:28 +0530

    Login Backend Connected

--- Functions/test.php
+++ Functions/test.php
@@ -7,7 +7,25 @@
      <title>Document</title>
 </head>
 <body>
-     <?php include("functions.php")?>
-     <h2><?php getCrops(); ?></h2>
+     <?php
+               session_start();
+                    include("../Includes/db.php");
+                    if(!isset($_SESSION['phonenumber'])) {
+                    echo "<h1>Not Login</h1>";
+                    }
+                    else {
+                         $phonenumber = $_SESSION['phonenumber'];
+                         echo $phonenumber;
+                         $query = "select * from buyerregistration where buyer_phone = '$phonenumber'";
+                         $run_query = mysqli_query($con,$query);
+                         while($row = mysqli_fetch_array($run_query)){
+                             echo $row['buyer_name'];
+                              
+                         }
+                         // echo $name;
+                         // echo "<li id='newlabel'><label id='login'>Hello,</label></li>";
+                         // echo "<li id='newlabel'><label id='login'>" . $nametest . "</label></li>";
+               }
+     ?>
 </body>
 </html>

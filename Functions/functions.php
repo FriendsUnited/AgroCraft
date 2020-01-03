d367cdba6bdd8271c7be4b5a0b3d06d51a2f1cbd
Elysian Spirit <abhig0209@gmail.com>
2020-01-03 11:58:51 +0530

    testing

--- Functions/functions.php
+++ Functions/functions.php
@@ -57,7 +57,7 @@
 
     function getProducts() {
         global $con;
-        $query = "select * from products  order by RAND() LIMIT 0,8";
+        $query = "select * from products  order by RAND() LIMIT 0,10";
         $run_query = mysqli_query($con,$query);
         echo "<br>";
         while($rows = mysqli_fetch_array($run_query)){
@@ -67,26 +67,26 @@
             $product_delivery = $rows['product_delivery'];
 
            
-            echo "<div class='example'>
+            echo "
                     <div class='wrapper'>
                         <div class='inputwrapper'>
                             <br>
-                            <img src='./Admin/product_images/$product_image' alt= 'Image Not Available' onerror=this.src='./Images/Website/noimage.jpg' style='height: 100px; width: 100px;'><br><br>
+                            <a href='#'><img src='./Admin/product_images/$product_image' alt= 'Image Not Available' onerror=this.src='./Images/Website/noimage.jpg' style='height: 100px; width: 100px;'><br><br></a>
                             <label>$product_title</label><br>
                             <label>PRICE:- $product_price Rs/kg</label><br>	
                             <label id='shop2'>Delivery by Farmer</label><br>Qty:-
                             <input class='numberinput' type='number' name='number'  >
                             <button class='addtocart'>ADD TO CART <i class='fas fa-shopping-cart' style=' background-color:#FFD700'></i></button><br><br>
                         </div>      
-                </div></div>";
+                </div> ";
 
         }
     }
 
-
-    function getVegetablesHomepage(){
+    
+    function getFruitsHomepage(){
         global $con;
-        $query = "select * from products where product_cat = 2 order by RAND() LIMIT 0,4";
+        $query = "select * from products where product_cat = 3 and not (product_image = '') order by RAND() LIMIT 0,5";
         $run_query = mysqli_query($con,$query);
         while($rows = mysqli_fetch_array($run_query)){
             $product_title = $rows['product_title'];
@@ -96,7 +96,27 @@
             $product_cat = $rows['product_cat'];
 
             echo "<div class='veg'>
-                    <img src='Admin/product_images/$product_image' height='250px' width='300px' >
+                    <a href='../index.html'><img src='Admin/product_images/$product_image' height='250px' width='300px'></a>
+                    </div>";
+            
+            
+        }
+    }
+
+
+    function getVegetablesHomepage(){
+        global $con;
+        $query = "select * from products where product_cat = 2 and not (product_image = '') order by RAND() LIMIT 0,4";
+        $run_query = mysqli_query($con,$query);
+        while($rows = mysqli_fetch_array($run_query)){
+            $product_title = $rows['product_title'];
+            $product_image = $rows['product_image'];
+            $product_price = $rows['product_price'];
+            $product_delivery = $rows['product_delivery'];
+            $product_cat = $rows['product_cat'];
+
+            echo "<div class='veg'>
+                    <a href='#'><img src='Admin/product_images/$product_image' height='250px' width='300px' ></a>
                     </div>";
             
             
@@ -105,7 +125,7 @@
 
     function getCropsHomepage(){
         global $con;
-        $query = "select * from products where product_cat = 1 order by RAND() LIMIT 0,4";
+        $query = "select * from products where product_cat = 1 and not (product_image = '') order by RAND() LIMIT 0,4";
         $run_query = mysqli_query($con,$query);
         while($rows = mysqli_fetch_array($run_query)){
             $product_title = $rows['product_title'];
@@ -115,7 +135,7 @@
             $product_cat = $rows['product_cat'];
 
             echo "<div class='veg'>
-                    <img src='Admin/product_images/$product_image' height='250px' width='300px' >
+                    <a href='#'><img src='Admin/product_images/$product_image' height='250px' width='300px' ></a>
                     </div>";    
         }
     }

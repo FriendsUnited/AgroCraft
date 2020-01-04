269e0e350fb68b2a496bec0426292dc285d58d30
hated_one <anshmiteshchhadva@gmail.com>
2020-01-04 19:25:36 +0530

    homepage almost completed

--- BuyerPortal/BuyerHomepage.php
+++ BuyerPortal/BuyerHomepage.php
@@ -10,7 +10,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<title>Agrocraft Homepage</title>
-	<link rel="stylesheet" type="text/css" href="../Styles/BuyerHomepage.css">
+	<!-- <link rel="stylesheet" type="text/css" href="../Styles/BuyerHomepage.css"> -->
 	<!-- <link rel="stylesheet" href="portal_files/font-awesome.min.css"> -->
 	<!-- <script src="../portal_files/c587fc1763.js.download" crossorigin="anonymous"></script> -->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -21,8 +21,386 @@
   	<script src="../portal_files/popper.min.js.download"></script>
 	<script src="../portal_files/bootstrap.min.js.download"></script>
 	  
-	
+	<style>
+			*{
+			margin: 0;
+			box-sizing:border-box;
+		}
+		
 
+		.header{
+			position: sticky;
+			z-index:100;
+			top :0rem;
+		height: 69px;
+		width: 100%;     
+		background-color:  #00b300  ;
+		}
+		.select_element{
+		width:20px;
+		background-color:transparent;
+		border:none;
+		text:none;
+		}
+
+	
+		#logo {
+			height: 66px;
+			width: 100px;
+			text-align: left;
+			float: left;
+		}
+		.search_input{
+		float:left;
+		margin-left: 50px;
+		margin-top: 20px;
+		
+		}
+		.proicon{
+		float: right;
+		margin-right: 10px;
+		margin-top: 20px;
+		}
+		.dropdown{	float: right;
+		margin-right: 10px;
+		margin-top: 20px;
+
+
+		}
+		.options{
+			/* font-weight:bold; */
+			color:yellow;
+			margin-left:20px;
+			width:20px;
+			
+		 } 
+		.icon2{
+		float: right;
+		margin-right: 10px;
+		margin-top: 20px;
+
+		}
+		#login{
+			float:left ;
+		/* margin-right: -70px; */
+		
+		margin-left: 870px;
+		/* margin-right: 10px; */
+		margin-top: 20px;
+		
+
+		}
+		 .headerdown{
+			height:50px;
+			width:100%;
+			/* background:red; */
+			
+		}
+		.makeitgreen{
+			color:  #00b300;
+		}
+
+.sel1{
+	/* border-style:groove; */
+    color:green;
+	float:left;
+	/* border-color: #00b300; */
+	/* border-radius:25%; */
+	margin-top:3px;
+}
+	.sel2{
+		border-color:green;
+		color:green;
+		float:left;
+		margin-left:600px;
+		/* border-style:groove; */
+		/* border-color: #00b300; */
+		/* border-radius:25%; */
+		margin-top:3px;
+	}
+	
+	.sel3{
+		font-size:20px;
+		margin-top:3px;
+		float:right;
+		margin-right:5px;
+		/* border-style:groove; */
+		/* border-color: #00b300; */
+		/* border-radius:25%; */
+		
+	}
+
+		.slideshow{
+			margin-top: 10px;
+			margin-left: 100px;
+			margin-bottom: 20px;
+			/*background-color: black;*/
+		}
+
+
+		#input1 {
+			width:220px;
+			border:none;
+
+			}
+
+
+		#input1:active {
+			background-color: tomato;
+
+		}
+
+		#opt {
+			height: 50px;
+			width: 100%;
+			background-color: red;
+			padding-top: 0px;
+			margin-top: 0px;
+		}
+
+		#bas {
+			float: left;
+			/*margin-top: 15px;*/
+			background-color: #FFD700;
+			height: 50px;
+		}
+
+
+
+		#abc {
+			float: right;
+			/*margin-top: 15px;*/
+			background-color: #FFD700;
+			height: 50px;
+		}
+		#cde {
+			float: left;
+			margin-left: 680px;
+			/*margin-top: 15px;*/
+			background-color: #FFD700;
+			height: 50px;
+		}
+
+
+		.carousel-indicators {
+			list-style: none;
+			background-color: transparent;
+		}
+
+		.carousel-item {
+			/* margin-left: 110px;
+		margin-top: 25px;
+			align-items: center;
+		justify-content: center;
+			padding-left: 110px;
+			padding-top: 100px;
+			overflow: hidden;
+			display: grid;
+			grid-template-columns: 80%; */
+		}
+
+		.carousel-control-prev {
+			margin-left: 60px;
+			/*color: red;*/
+			margin-top: 10px;
+		}
+
+		.carousel-control-next{
+			margin-right: 60px;
+		}
+
+		.wrapper
+		{
+			display: grid;
+			grid-template-columns: 20% 20% 20% 20%;
+
+
+			grid-column-gap: 20px;
+			grid-row-gap: 10px;
+		grid-column-gap: 20px;
+		grid-row-gap: 10px;
+		margin-left: 30px;
+
+		}
+
+		.inputwrapper{
+
+
+		float: left;
+		border-style: double;
+		text-align: center;
+		margin-left: 80px;
+		width: 300px;
+		margin-bottom:20px;
+		clear:auto;
+		}
+		.inputwrapper:last-child{
+			grid-column-gap: 20px;
+			grid-row-gap: 10px;
+			margin-left: 30px;
+		}
+
+		.inputwrapper{
+			float: left;
+			border-style: double;
+			text-align: center;
+			margin-left: 80px;
+			width: 300px;
+			margin-bottom:20px;
+			clear:auto;
+			}
+
+
+		.inputwrapper:last-child
+		{
+		margin-right:30px; 
+			/*background-color: red;*/
+		}
+
+		.carousel-control-next {
+			margin-right: 60px;
+		}
+
+
+
+		.addtocart{
+			background-color: #FFD700;
+		}
+
+		.numberinput{
+		width:35px;
+
+		}
+
+		.tabless{
+			border-collapse: separate ;
+			border-spacing: 20px;
+			padding: 20px;
+			border-style: solid;
+			margin: 10px;
+			/*background-color: red;*/
+			border-width: 5px;
+					border-radius: 16px;
+					border-color:black;
+					text-align: center;
+					align-items: center;
+		min-width: 500px;
+		margin-left:100px;
+		}
+
+
+		.picha{
+			height: 300px;
+		width: 500px;
+		background-color: red;
+		}
+		.images{
+		min-width: 400px;
+			box-shadow: 5px 5px 10px grey;
+		}
+
+		.images1{
+
+		}
+		#heading {
+			font-size: 20px;
+			color: white;
+			text-align: center;
+			background-color: black;
+			justify-content: center;
+		}
+
+		.head1 {}
+
+		.addtocart {
+			background-color: #FFD700;
+		}
+		.bestselers{
+			background-color: red;
+			border-style: solid;
+			float: left;
+			margin-top: 10px;
+			/*margin: 20px;*/
+		margin-bottom: 10px;
+			margin-right: 40px;
+			margin-left: 45px;}
+		.bestselers:last-child{
+		margin-right: 50px;
+		}
+
+
+		#fruits{
+			text-align: center;
+		}
+		.veg{
+		
+			float:left;
+
+			margin-left: 30px;
+			border-style: solid;
+			margin-top: 10px;
+		}
+		#vegs{
+		height: 270px;
+			width: 1498px;
+			/*margin: 20px;*/
+			margin-right: 10px;
+			margin-left: 10px;
+			/*background-color: blue;*/
+			border-style: double;
+			margin-top: 20px;
+
+		}
+		.veg:first-child{
+			margin-left:80px;
+		}
+		.veg:last-child{
+			margin-right: 20px;
+		}
+		#crops{
+		height: 270px;
+			width: 1498px;
+			/*margin: 20px;*/
+			margin-right: 10px;
+			margin-left: 10px;
+			/*background-color: blue;*/
+			border-style: double;
+			margin-top: 20px;
+
+		}
+		.crop:first-child{
+			margin-left:80px;
+		}
+		.crop:last-child{
+			margin-right: 20px;
+		}
+		.crop{
+		/*    height: 210px;
+			width: 300px;
+		*/
+			float:left;
+		/*  height: 200px;
+			width: 300px;*/
+			margin-left: 30px;
+			border-style: solid;
+			margin-top: 10px;
+		}
+		.content_item{
+		text-align: center;
+		justify-content: center;
+
+		}
+		.etc{
+		margin-left:-40px;
+			min-width:90px;
+			font-size:20px;
+			
+		}
+		.crop_items{
+			color:green;
+		}
+		
+			</style>
 </head>
 <body>
 
@@ -40,10 +418,26 @@
 		<i class="fas fa-search" style="font-size:20px;color:white; " ></i>
 		<input type="text" id="input1" name="search" placeholder="Search..">
 	</div>
+	<div class="dropdown">
+		<!-- <select class="select_element" name="drop">
+			 <option class="options option1" value="0"></option> -->
+			<!-- <option  class="options " value="profle"><label class="abcde">PROFILE</label></option>
+			<option class="options "value="logout">LOGOUT</option>
+			 -->
+		<!-- </select>  -->
 
-
+		<button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">	</span></button>
+    <ul class="dropdown-menu etc" >
+      <li  class="options" role="presentation"><a role="menuitem" tabindex="-1" href="#"><label class="makeitgreen" >Profile</label></a></li>
+      <li class="options" role="presentation"><a role="menuitem" tabindex="-1" href="#"><label class="makeitgreen">Logout</label></a></li>
+      
+	</div>	
 	<div class="proicon">
-		<a href="BuyerProfile.php"> <i class="far fa-user-circle" style="font-size:30px; color: white"></i></a>
+		
+    <i class="far fa-user-circle" style="font-size:30px; color: white"></i>
+
+
+		<!-- <a href="BuyerProfile.php"> <i class="far fa-user-circle" style="font-size:30px; color: white"></i></a> -->
 	</div>
 
 	<div class="icon2">
@@ -58,7 +452,34 @@
 	</div>
 </div>
 
+<div class="headerdown">
+<div class="sel1 sel">
+<button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">	CROPS
+     <span class="caret"></span></button>
+   	 <ul class="dropdown-menu  " >
+      	<li  class="options crop_items" role="presentation"><a role="menuitem" tabindex="-1" href="#">
+			  <label class="crop_items">Rice</a></li></label>
+		 <li class="options" role="presentation"><a role="menuitem" tabindex="-1" href="#"> <label class="crop_items">Bajra</label></a></li>
+		 </div>
+<div class="sel2 sel">
+		 <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">VEGETABLES	</i>
+	 <span class="caret"></span></button>
+   	 <ul class="dropdown-menu " >
+     	<li  class="options" role="presentation"><a role="menuitem" tabindex="-1" href="#"> <label class="crop_items">POTAT0</label></a></li>
+		  <li class="options" role="presentation"><a role="menuitem" tabindex="-1" href="#"> <label class="crop_items">BROCOLI</label></a></li></ul>
+</div>	
+<div class="sel3 sel"	>  
+         <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">FRUITS	</i>
+   	   <span class="caret"></span></button>
+   	   <ul class="dropdown-menu " >
+         <li  class="options" role="presentation"><a role="menuitem" tabindex="-1" href="#"> <label class="crop_items">CHICKOO</label></a></li>
+         <li class="options" role="presentation"><a role="menuitem" tabindex="-1" href="#"> <label class="crop_items">ORNAGE</label></a></li></ul>
+      	</div>
 
+
+
+
+	</div>
 
  
  <div class="slideshow">

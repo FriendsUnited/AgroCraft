f36afd176cd5296e5ecc841736b5503abf4352e2
AB <arpitbhat48@gmail.com>
2020-01-08 23:36:46 +0530

    changes

---
+++ loctst.php
@@ -0,0 +1,22 @@
+<?php include("Functions/functions.php")?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <button onclick = "getLocation()"> noobnoob </button>
+    <div id="demo">
+
+    </div>
+
+    <script>
+    var x = document.getElementById("demo");
+    showPosition(position);
+</script>
+</body>
+</html>

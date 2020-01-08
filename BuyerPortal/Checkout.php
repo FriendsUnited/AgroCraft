ff4bae9886e6cf7c9cd7adaf1e9f0a24b3e33716
Elysian Spirit <abhig0209@gmail.com>
2020-01-09 01:53:40 +0530

    backend on process

--- BuyerPortal/Checkout.php
+++ BuyerPortal/Checkout.php
@@ -1,196 +1,197 @@
+<?php
+include('../Functions/functions.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<!-- <link rel="stylesheet" type="text/css" href="cart_pages.css">
- -->    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://kit.fontawesome.com/c587fc1763.js" crossorigin="anonymous"></script>
-    <title>checkout </title>
-    <style>.tabley {
-               width: 100%;
-               margin-right:20px;
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <script src="https://kit.fontawesome.com/c587fc1763.js" crossorigin="anonymous"></script>
+  <title>checkout </title>
+  <style>
+    .tabley {
+      width: 100%;
+      /* margin-right: 20px; */
+    }
 
-               /*border-style: solid;*/
-               /*border-width: 2px;*/
-               /*padding: 25px;*/
-/*                margin-right:215px; 
-*/          }
+    .tabley th {
+      text-align: center;
+      border-style: solid;
+      padding: 5px;
+      margin: 5px;
+    }
 
-          .tabley th {
-               text-align: center;
-               border-style: solid;
-               /*border-width: 2px;*/
-               padding: 5px;
-               margin: 5px;
+    .tdy {
+      text-align: center;
+      border-style: solid;
+      border-width: 2px;
+      padding: 10px;
+    }
 
-               
-               
-              
-          }
+    th {
+      background-color: #555;
+      color: white;
+    }
 
-          .tdy {
-               text-align: center;
-               border-style: solid;
-               border-width: 2px;
-               padding: 10px;
+    * {
+      margin: 0;
+    }
 
-          }
+    .add {
+      background-color: #f5ca0a;
+      border-style: solid;
+      border-width: 2px;
+      color: black;
+      border-radius: 25%;
+      padding: 20px;
+      margin-left: 10px;
+      margin-top: 5px;
+      width: 100px;
+      clear: both;
+      text-align: center;
 
-          th{
-               background-color: #555;
-               color: white;
-          }
+    }
 
-*{
-  margin:0;
-}
+    .edit {
+      margin-left: 550px;
+      background-color: #f5ca0a;
+      border-style: solid;
+      border-width: 2px;
+      color: black;
+      border-color: black;
+      border-radius: 25%;
+      padding: 20px;
+      text-align: center;
+      margin-bottom: 18px;
+    }
 
-.add{
-  /*float:left;*/
-  background-color: #f5ca0a;
-border-style: solid;
-border-width: 2px;
-color:black;
-border-radius:25%;
-padding: 20px;
-margin-left:10px;
-margin-top:5px;
-width:100px;
-clear: both;
-  text-align: center;
+    .place_odder {
+      text-align: center;
+      margin-top: 20px;
+    }
 
-}
-.edit{
-  margin-left:550px;
-   background-color: #f5ca0a;
-border-style: solid;
-border-width: 2px;
-color:black;
-border-color: black;
-border-radius:25%;
-padding: 20px;
-  text-align: center;
-  margin-bottom: 18px;
-/*margin-top: -50px;
-*/
-}
-.place_odder{
-  text-align: center;
-  margin-top: 20px;
-}
-.lang{
-  width: 20%;
-  text-align: center;
-  height:100px;
-  background-color: white;
-  margin-left: 195px;
-  margin-top: -120px;
-border-style: solid;
-border-width: 2px;
-padding:10px;
-justify-content: center;
-border-radius:5%;
-border-color:black ;
-}
-.item{
-  float:left;
-  margin-top:-70px;
-  background-color: #f5ca0a;
-border-style: solid;
-border-width: 2px;
-color:black;
-border-radius:25%;
-padding: 20px;
-margin-left:10px;
-}
-         .payment { background-color: #f5ca0a;
-border-style: solid;
-border-width: 2px;
-color:black;
-border-color: black;
-border-radius:25%;
-padding: 20px;
-  text-align: center;
-               float: left;
-               clear: both;
-               margin-left: 0px;
-               font-size: 20px;
-               margin-top: 20px;
-          }
+    .lang {
+      width: 20%;
+      text-align: center;
+      height: 100px;
+      background-color: white;
+      margin-left: 195px;
+      margin-top: -120px;
+      border-style: solid;
+      border-width: 2px;
+      padding: 10px;
+      justify-content: center;
+      border-radius: 5%;
+      border-color: black;
+    }
 
-          .cash {   clear: both;
+    .item {
+      float: left;
+      margin-top: -70px;
+      background-color: #f5ca0a;
+      border-style: solid;
+      border-width: 2px;
+      color: black;
+      border-radius: 25%;
+      padding: 20px;
+      margin-left: 10px;
+    }
 
-               float: left;
-               margin-top: 10px;
-               margin-left: 220px;
-               margin-right: 20px;
-          }
+    .payment {
+      background-color: #f5ca0a;
+      border-style: solid;
+      border-width: 2px;
+      color: black;
+      border-color: black;
+      border-radius: 25%;
+      padding: 20px;
+      text-align: center;
+      float: left;
+      clear: both;
+      margin-left: 0px;
+      font-size: 20px;
+      margin-top: 20px;
+    }
 
-          .patym { margin-top: 10px;
-               margin-left: 20px;
+    .cash {
+      clear: both;
 
-               float: left;
+      float: left;
+      margin-top: 10px;
+      margin-left: 220px;
+      margin-right: 20px;
+    }
 
-          }
-          .del_options{
-            margin-left: 130px;
-            margin-right: 25px;
+    .patym {
+      margin-top: 10px;
+      margin-left: 20px;
 
-          }
+      float: left;
 
-.footer{
-  height:100px;
-  width:100%;
-}
-.a{
-  height:100px;
-  width:100%;
-}
-.selly{
-  text-align: center;
-  width:100%;
-  height: 40px;
-}
-.place_odder{
-float:right;
-border-color: black;
- background-color: #f5ca0a;
-border-style: solid;
-border-width: 2px;
-color:black;
-border-radius:25%;
-padding: 20px;
-margin-left:10px;
-margin-right:20px;
-}
+    }
 
-.boxy {
-               height: 150px;
-               width: 20%;
-               float: right;
-                 
-               margin-top: -490px;
-               margin-right: 20px;
-             
-          }
- .totaly {background-color: #f5ca0a;
-border-style: solid;
-border-width: 2px;
-color:black;
-border-radius:25%;
-               float: left;
-               text-align: center;
-               /*justify-content: center;*/
-               font-size: 25px;
-               padding: 20px;
-               /*margin-top:120px;
-     margin-left:20px; */
-          }
-             .rs {
-               margin-left: 10px;
-               float: right;
-          }
+    .del_options {
+      margin-left: 130px;
+      margin-right: 25px;
+
+    }
+
+    .footer {
+      height: 100px;
+      width: 100%;
+    }
+
+    .a {
+      height: 100px;
+      width: 100%;
+    }
+
+    .selly {
+      text-align: center;
+      width: 100%;
+      height: 40px;
+    }
+
+    .place_odder {
+      float: right;
+      border-color: black;
+      background-color: #f5ca0a;
+      border-style: solid;
+      border-width: 2px;
+      color: black;
+      border-radius: 25%;
+      padding: 20px;
+      margin-left: 10px;
+      margin-right: 20px;
+    }
+
+    .boxy {
+      height: 150px;
+      width: 20%;
+      float: right;
+      margin-top: -800px;
+      margin-right: 20px;
+    }
+
+    .totaly {
+      background-color: #f5ca0a;
+      border-style: solid;
+      border-width: 2px;
+      color: black;
+      border-radius: 25%;
+      float: left;
+      text-align: center;
+      font-size: 25px;
+      padding: 20px;
+    }
+
+    .rs {
+      margin-left: 10px;
+      float: right;
+    }
 
     h3 {
       width: 100%;
@@ -253,137 +254,172 @@ border-radius:25%;
     .copy {
       float: left;
       margin-top: -65px;
-      /* background-color:grey; */
     }
-    .bott{
-      /*background-color: red;*/
-      height:100px;
 
-      width:100%;
+    .bott {
+      height: 100px;
+
+      width: 100%;
     }
-    .scam{
-      margin-top:20px;
-      margin-left:30px;
+
+    .scam {
+      margin-top: 20px;
+      margin-left: 30px;
     }
-    .header{
+
+    .header {
       font-family: 'Courgette', cursive;
       font-size: 50px;
       text-align: center;
     }
-      .leaf{
-          color:#00b300;
-        }
-        .images{
-          height: 100px;
-          width:100px;
-          margin-top: 10px;
-/*margin-left: 100px;
-*/        }
-        .pay{
-          font-size: 20px;
-        }
-    .hypher{
+
+    .leaf {
+      color: #00b300;
+    }
+
+    .images {
+      height: 100px;
+      width: 100px;
+      margin-top: 10px;
+    }
+
+    .pay {
+      font-size: 20px;
+    }
+
+    .hypher {
       margin-right: 20px;
     }
+
     input[type=radio] {
-    border: 0px;
-    width: 1.5%;
-    height: 35px;
-    margin-left: 100px;
+      border: 0px;
+      width: 1.5%;
+      height: 35px;
+      margin-left: 100px;
 
-}
-.radios{
-  margin-top: -30px;
-}
+    }
 
-</style>
-   <link href="https://fonts.googleapis.com/css?family=Courgette&display=swap" rel="stylesheet">
+    .radios {
+      margin-top: -30px;
+    }
+  </style>
+  <link href="https://fonts.googleapis.com/css?family=Courgette&display=swap" rel="stylesheet">
 </head>
+
 <body>
   <div class="header">AGROCRAFT <i class="fas leaf fa-leaf"></i></div>
-    <br>
-<div class="add">
-	<label>1.</label><label class="del_add">DELIVERY ADDRESS</label></div>
-	<div class="sam">	<button class="edit">CHANGE  ADDRESS</button></div>
-		<br>
-<div class="lang">
+  <br>
+  <div class="add">
+    <label>1.</label><label class="del_add">DELIVERY ADDRESS</label></div>
+  <div class="sam"> <button class="edit">CHANGE ADDRESS</button></div>
+  <br>
+  <div class="lang">
 
-<label>lorem ipsum knwo dnqwo dnkqwdhqwh dqoh9oh dnqohi <br>lorem ipsum knwo dnqwo dnkqwdhqwh dqoh9oh dnqohi </label>
-</div>
+    <label>lorem ipsum knwo dnqwo dnkqwdhqwh dqoh9oh dnqohi <br>lorem ipsum knwo dnqwo dnkqwdhqwh dqoh9oh dnqohi </label>
+  </div>
 
-<br>
-<br>
-<br>
-<br>
-<br>
-<label class= " item">2.ITEMS</label>
-<div class="del_options">
-     <table class="tabley">
-          <thead>
-               <th class="thy">Sr no</th>
-               <th class="thy">Item Name</th>
-                   <th class="thy">Quantity</th>
-           
-               <th class="thy">SubTotal</th>
+  <br><br><br><br><br>
 
-               <th class="thy">Delivary Options</th>
-                        </thead>
- <tr>
-               <td class="tdy" data-label="Sr no">1</td>
-               <td class="des tdy" data-label="Item Name">Best Patato of the world
-               </td>
-               <td class="tdy" data-label="Quantity">2</td>
 
-               <td class="tdy" data-label="Unit Price">rs 2</td>
-              
-               <td class="tdy" data-label="Deletion">
-               	<select class="selly">
-  <option  value="Buyer">Buyer</option>
-  <option value="Farmer">Farmer</option>
-  <option value="Courier">Courier</option>
-</select>
-               </td></tr>
-                <tr>
-               <td class="tdy" data-label="Sr no">1</td>
-               <td class="des tdy" data-label="Item Name">Best Patato of the world
-               </td>
-                 <td class="tdy" data-label="Quantity">2</td>
-               <td class="tdy" data-label="Unit Price">rs 2</td>
-              
-               <td class="tdy" data-label="Deletion">
+
+
+
+
+
+
+  <label class=" item">2.ITEMS</label>
+
+  <div class="del_options">
+    <table class="tabley">
+      <thead>
+        <th class="thy">Sr no</th>
+        <th class="thy">Item Name</th>
+        <th class="thy">Quantity</th>
+        <th class="thy">SubTotal</th>
+        <th class="thy">Delivary Options</th>
+      </thead>
+      <?php
+      global $con;
+      if (isset($_SESSION['phonenumber'])) {
+        $sess_phone_number = $_SESSION['phonenumber'];
+        $sel_price = "select * from cart where phonenumber = '$sess_phone_number'";
+        $run_price = mysqli_query($con, $sel_price);
+        $i = 0;
+        while ($p_price = mysqli_fetch_array($run_price)) {
+          $product_id = $p_price['product_id'];
+          $qty = $p_price['qty'];
+          $subtotal = $p_price['subtotal'];
+          $pro_price = "select * from products where product_id='$product_id'";
+          $run_pro_price = mysqli_query($con, $pro_price);
+          while ($pp_price = mysqli_fetch_array($run_pro_price)) {
+            $product_title = $pp_price['product_title'];
+            $product_price = $pp_price['product_price']; ?>
+            <tr>
+              <td class="tdy" data-label="Sr no"><?php echo $i; ?></td>
+              <td class="des tdy" data-label="Item Name"><?php echo $product_title; ?> </td>
+              <td class="tdy" data-label="Quantity"><?php echo $qty; ?></td>
+              <td class="tdy" data-label="Unit Price">Rs <?php echo $product_price; ?></td>
+              <td class="tdy" data-label="Deletion">
+              <form action="" method = 'post'>
                 <select class="selly">
-  <option  value="Buyer">Buyer</option>
-  <option value="Farmer">Farmer</option>
-  <option value="Courier">Courier</option>
-</select>
-               </td></tr>
-                 </table></div>
-<div class="bott">
-          
-          
-     
-    <button class="place_odder">PLACE ORDER</button>
+                  <option name = 'delivery' value="Buyer">Buyer</option>
+                  <option name = 'delivery' value="Farmer">Farmer</option>
+                  <option name = 'delivery' value="Courier">Courier</option>
+                </select>
+              </td>
+            </tr>
 
-    <button class="place_odder">GO BACK</button>
-</div>
-                
-<div class="up">
 
-          <div class="boxy">
-               <label class="totaly"> GRAND TOTAL : <label class="rs">Rs 35</label></label>
-               
-                <label class="totaly scam">TOTAL ITEMS :<label class="rs">5</label></label>
-          </div>
+      <?php
+          }
+          $i++;
+        }
+      } ?>
+    </table>
+  </div>
 
-     
 
-     </div>
-     <div class="delivary">
-       <label class="totaly pay">3.PAYMENT OPTIONS:</label>
-       <input type="radio" class="radios" name="gender" value="male"> <img class ="images" src="../Images/Website/cash.jpg">
-<input type="radio" class="radios" name="gender" value="female"> <img class ="images" src="../Images/Website/petm.jpg ">
-     </div>
-<h3><span> GET SOCIAL WITH US </span></h3>
+  <br><br><br>
+
+
+  <div class="delivary">
+    <label class="totaly pay">3.PAYMENT OPTIONS:</label>
+    <input type="radio" class="radios" name="payment" value="cod"> <img class="images" src="../Images/Website/cash.jpg">
+    <input type="radio" class="radios" name="payment" value="paytm"> <img class="images" src="../Images/Website/petm.jpg ">
+  </div>
+
+
+  <div class="bott">
+
+    <button name = 'order' class="place_odder">PLACE ORDER</button>
+    </form>
+    <?php
+      if (isset($_POST['order']))
+      {
+          $payment = $_Post['payment'];
+          $delivery = $_POST['delivery'];
+          $total = $_SESSION['grandtotal'];
+          $query = "insert into order (address,delivery,phonenumber,grandtotal,payment) values 
+          '$address','$delivery','$sess_phone_number','$total','$payment' ";
+          $run_query = mysqli_query($con,$query);
+      }
+    ?>
+    <a href="CartPage.php"><button class="place_odder">GO BACK</button></a>
+  </div>
+
+  <div class="up">
+
+    <div class="boxy">
+      <label class="totaly"> GRAND TOTAL : <label class="rs">Rs <?php echo $_SESSION['grandtotal'] ?></label></label>
+
+      <label class="totaly scam">TOTAL ITEMS :<label class="rs"><?php echo totalItems(); ?></label></label>
+    </div>
+
+
+
+  </div>
+
+  <h3><span> GET SOCIAL WITH US </span></h3>
   </div>
 
   <div class="morefooter">
@@ -412,9 +448,10 @@ border-radius:25%;
     <h5 style="test-align:center; padding-top:40px;margin-left:650px; background-color:'grey';">Copyright &copy; 2022 by www.AgroCraft.com</h5>
   </div>
 
- 
+
 
 
 
 </body>
+
 </html>
